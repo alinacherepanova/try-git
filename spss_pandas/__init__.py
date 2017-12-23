24a5f58e908917475325614381ecee2f06a573fa
Alina Cherepanova <aicherepanova@edu.hse.ru>
2017-12-23 18:26:22 +0000

    Yo;

---
+++ spss_pandas/__init__.py
@@ -0,0 +1,14 @@
+from rpy2.robjects import r, pandas2ri
+from rpy2.robjects.packages import importr
+
+foreign = importr('foreign')
+
+def sav_to_pandas(fn):
+    r_df = foreign.read_spss(fn,
+                           use_value_labels=True,
+                           to_data_frame=True)
+    py_df = pandas2ri.ri2py(r_df)
+    return py_df
+
+def print_version():
+    print('0.0.1')
